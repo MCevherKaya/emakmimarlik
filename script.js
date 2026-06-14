abbaf4937095f36cbd96f2e5fb19425116da24a1
Münibe Cevher Kaya <munibecevher@icloud.com>
2026-06-15 01:13:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-const btn=document.querySelector('.menu-toggle');
-const nav=document.querySelector('.nav-links');
-btn?.addEventListener('click',()=>nav.classList.toggle('show'));
-document.querySelectorAll('.nav-links a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('show')));
+const menuToggle=document.querySelector(".menu-toggle");const navLinks=document.querySelector(".nav-links");menuToggle?.addEventListener("click",()=>{navLinks.classList.toggle("show")});document.querySelectorAll(".nav-links a").forEach(link=>{link.addEventListener("click",()=>{navLinks.classList.remove("show")})});
