149c10776ccee06fb18c375bd63e360da189760e
Münibe Cevher Kaya <munibecevher@icloud.com>
2026-06-29 02:36:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -134,10 +134,10 @@ document.querySelectorAll(".hero-video-frame").forEach((frame) => {
 
   const toggleSound = () => {
     video.muted = !video.muted;
-    video.volume = 1.0;
+    video.volume = 0.65;
     video.play().catch(() => {});
     updateText();
-};
+  };
 
   toggle.addEventListener("click", (e) => {
     e.stopPropagation();
@@ -145,16 +145,13 @@ document.querySelectorAll(".hero-video-frame").forEach((frame) => {
   });
 
   video.addEventListener("click", toggleSound);
-  video.muted = true;
-  video.volume = 1.0;
-  video.play().catch(() => {});
   updateText();
 });
+
 
 /* FORCE HERO VIDEO AUTOPLAY */
 window.addEventListener("load", () => {
   const video = document.querySelector(".hero-video");
-
   if (!video) return;
 
   video.muted = true;
